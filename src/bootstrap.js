18c8490dbd4853908d95439744ca8c5f2f7c4110
deosidd <Siddarth.deo@carrier.com>
2023-03-03 12:55:38 +0530

    Added mount function

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,16 @@
-import faker from 'faker';
+import faker from "faker";
 
-const cartText = `<h4>Welcome to Cart MFE</h4> <div>you have ${faker.random.number()} item in cart</div>`
-document.querySelector("#dev-cart").innerHTML = cartText;
+const mount = (el) => {
+  const cartText = `<h4>Welcome to Cart MFE</h4> <div>you have ${faker.random.number()} item in cart</div>`;
+  el.innerHTML = cartText;
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-cart");
+  /** Assuming dev-products element is not included in container app */
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
